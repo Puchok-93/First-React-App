b2b0c0ec33814f1a3fdb396f9f2d643eb48ebead
Puchok-93 <54364298+Puchok-93@users.noreply.github.com>
2022-10-14 18:06:38 +0300

    Добавление/удаление элементов из списка

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,4 @@
+import {Component} from 'react';
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -12,20 +13,57 @@ const data = [
     {name: 'Alex' , salary: 2000, increase: false, id: 3},
 ]
 
-function App() {
-  return (
-    <div className="app">
-        <AppInfo />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {name: 'John', salary: 800, increase: false, id: 1},
+        {name: 'Bruce',  salary: 1500, increase: false, id: 2},
+        {name: 'Alex' , salary: 2000, increase: false, id: 3},
+      ],
+    }
+    this.maxId = 4;
+  }
 
-        <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
-        </div>
-        
-        <EmployeesList data={data}/>
-        <EmployeesAddForm/>
-    </div>
-  );
+  deleteItem = (id) => {
+    this.setState(({data}) => {
+      return {
+        data: data.filter(item => item.id !== id) 
+      }
+    })
+  }
+
+  addItem = (name, salary) => {
+    const newItem = {
+      name: name,
+      salary: salary,
+      increase: false,
+      id: this.maxId + 1,
+    }
+    this.setState(({data}) => {
+      const newArr = [...data, newItem];
+      return {
+        data: newArr,
+      }
+    });
+  }
+
+  render(){
+    return (
+      <div className="app">
+          <AppInfo />
+  
+          <div className="search-panel">
+              <SearchPanel/>
+              <AppFilter/>
+          </div>
+          
+          <EmployeesList data={this.state.data} onDelete={this.deleteItem}/>
+          <EmployeesAddForm onAdd={this.addItem}/>
+      </div>
+    );
+  }
 }
 
 export default App;
